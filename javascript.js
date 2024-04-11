faded6d85e1884a383739b55dcfc9541bb4bf1d6
phani71995 <phani71995@gmail.com>
2024-04-12 00:17:54 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menubar = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamurger-icon");
+    menubar.classList.toggle("open");
+    icon.classList.toggle("open")
+
+
+
+
+
+
+
+
+
+
+}
